1849f9db123b839570fd4fb9fd9359efc25288dd
Rafael Haenel <rhaenel@quera.com>
2025-11-03 17:48:04 -0800

    All errors

--- pyzx/simulation/random.py
+++ pyzx/simulation/random.py
@@ -1,120 +1,130 @@
 import jax
 import jax.numpy as jnp
-
-key = jax.random.key(0)
-
-probs = jnp.array(
-    [
-        0.05,
-        0.1,
-        0.02,
-        0.03,
-        0.2,
-        0.1,
-        0.05,
-        0.05,
-        0.05,
-        0.05,
-        0.05,
-        0.05,
-        0.1,
-        0.02,
-        0.01,
-        0.00,
-    ]
-)
-probs /= probs.sum()
-
-logits = jnp.log(probs)
-
-N = 10000
-samples = jax.random.categorical(key, logits, shape=(N,))
-
-# optional: unpack to 4 bits
-bits = ((samples[:, None] >> jnp.arange(4)) & 1).astype(jnp.uint8)
-
-# print(bits)
+import abc
 
 
-def pauli_channel_1(px, py, pz, num_samples: int = 1):
-    # 00, 10, 01, 11
-    probs = jnp.array([1 - px - py - pz, pz, px, py])
-    logits = jnp.log(probs)
-    samples = jax.random.categorical(key, logits, shape=(num_samples,))
-    bits = ((samples[:, None] >> jnp.arange(2)) & 1).astype(jnp.uint8)
-    return bits
+class Channel(abc.ABC):
+    @abc.abstractmethod
+    def sample(self, num_samples: int = 1):
+        pass
 
 
-def pauli_channel_2(
-    pix=0,
-    piy=0,
-    piz=0,
-    pxi=0,
-    pxx=0,
-    pxy=0,
-    pxz=0,
-    pyi=0,
-    pyx=0,
-    pyy=0,
-    pyz=0,
-    pzi=0,
-    pzx=0,
-    pzy=0,
-    pzz=0,
-    num_samples: int = 1,
-):
-    remainder = (
-        1
-        - pix
-        - piy
-        - piz
-        - pxi
-        - pxx
-        - pxy
-        - pxz
-        - pyi
-        - pyx
-        - pyy
-        - pyz
-        - pzi
-        - pzx
-        - pzy
-        - pzz
-    )
-    probs = jnp.array(
-        [
-            remainder,  # 00,00
-            pzi,  # 10,00
-            pxi,  # 01,00
-            pyi,  # 11,00
-            piz,  # 00,10
-            pzz,  # 10,10
-            pxz,  # 01,10
-            pyz,  # 11,10
-            pix,  # 00,01
-            pzx,  # 10,01
-            pxx,  # 01,01
-            pyx,  # 11,01
-            piy,  # 00,11
-            pzy,  # 10,11
-            pxy,  # 01,11
-            pyy,  # 11,11
-        ]
-    )
-    logits = jnp.log(probs)
-    samples = jax.random.categorical(key, logits, shape=(num_samples,))
-    bits = ((samples[:, None] >> jnp.arange(4)) & 1).astype(jnp.uint8)
-    return bits
+class PauliChannel1:
+    def __init__(self, px: float, py: float, pz: float, key: jax.random.PRNGKey):
+        self._key = key
+        probs = jnp.array([1 - px - py - pz, pz, px, py])
+        self.logits = jnp.log(probs)
+
+    def sample(self, num_samples: int = 1):
+        self._key, subkey = jax.random.split(self._key)
+        samples = jax.random.categorical(subkey, self.logits, shape=(num_samples,))
+        bits = ((samples[:, None] >> jnp.arange(2)) & 1).astype(jnp.uint8)
+        return bits
 
 
-def x_error(p, num_samples: int = 1):
-    return
+class Depolarize1(PauliChannel1):
+    def __init__(self, p: float, key: jax.random.PRNGKey):
+        super().__init__(p / 3, p / 3, p / 3, key=key)
 
 
-print(pauli_channel_2(pxi=0.2, piy=0.6, num_samples=10))
+class PauliChannel2:
+    def __init__(
+        self,
+        pix: float,
+        piy: float,
+        piz: float,
+        pxi: float,
+        pxx: float,
+        pxy: float,
+        pxz: float,
+        pyi: float,
+        pyx: float,
+        pyy: float,
+        pyz: float,
+        pzi: float,
+        pzx: float,
+        pzy: float,
+        pzz: float,
+        key: jax.random.PRNGKey,
+    ):
+        self._key = key
+        remainder = (
+            1
+            - pix
+            - piy
+            - piz
+            - pxi
+            - pxx
+            - pxy
+            - pxz
+            - pyi
+            - pyx
+            - pyy
+            - pyz
+            - pzi
+            - pzx
+            - pzy
+            - pzz
+        )
+        probs = jnp.array(
+            [
+                remainder,  # 00,00
+                pzi,  # 10,00
+                pxi,  # 01,00
+                pyi,  # 11,00
+                piz,  # 00,10
+                pzz,  # 10,10
+                pxz,  # 01,10
+                pyz,  # 11,10
+                pix,  # 00,01
+                pzx,  # 10,01
+                pxx,  # 01,01
+                pyx,  # 11,01
+                piy,  # 00,11
+                pzy,  # 10,11
+                pxy,  # 01,11
+                pyy,  # 11,11
+            ]
+        )
+        self.logits = jnp.log(probs)
+
+    def sample(self, num_samples: int = 1):
+        self._key, subkey = jax.random.split(self._key)
+        samples = jax.random.categorical(subkey, self.logits, shape=(num_samples,))
+        bits = ((samples[:, None] >> jnp.arange(4)) & 1).astype(jnp.uint8)
+        return bits
 
 
-# depolarize_1(p)
-# [1, 0] p/3
-# [0, 1] p/3
-# [1, 1] p/3
+class Depolarize2(PauliChannel2):
+    def __init__(self, p: float, key: jax.random.PRNGKey):
+        super().__init__(
+            p / 15,
+            p / 15,
+            p / 15,
+            p / 15,
+            p / 15,
+            p / 15,
+            p / 15,
+            p / 15,
+            p / 15,
+            p / 15,
+            p / 15,
+            p / 15,
+            p / 15,
+            p / 15,
+            p / 15,
+            key=key,
+        )
+
+
+class Error(Channel):
+    def __init__(self, p: float, key: jax.random.PRNGKey):
+        self._key = key
+        self.p = p
+
+    def sample(self, num_samples: int = 1):
+        self._key, subkey = jax.random.split(self._key)
+        samples = jax.random.bernoulli(subkey, self.p, shape=(num_samples,)).astype(
+            jnp.uint8
+        )
+        return samples
